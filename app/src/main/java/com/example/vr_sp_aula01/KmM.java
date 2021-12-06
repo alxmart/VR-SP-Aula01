8ba701c3f77dab590219b2ea29f35ecc922d1a17
alxmart <alxmartinez@gmail.com>
2021-12-06 18:15:32 -0300

    Added a new activity (KmM)

---
+++ app/src/main/java/com/example/vr_sp_aula01/KmM.java
@@ -0,0 +1,14 @@
+package com.example.vr_sp_aula01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KmM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_km_m);
+    }
+}
